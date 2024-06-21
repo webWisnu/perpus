d079134478d72c1c4305adb679c5ed140912667e
Ardan Kusuma <143509388+webWisnu@users.noreply.github.com>
2024-06-21 14:18:49 +0700

    menambhakan seddder untuk role admin DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +17,31 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+               
+        schema::enableForeignKeyConstraints();
+       
+        Schema::disableForeignKeyConstraints();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+       $users = [
+            [
+                'username' => 'admin',
+                'slug' => 'admin',
+                'email' => 'admin@example.com',
+                'phone' =>  '098112347788',
+                'addres' => 'yogyakarta',
+                'role_id' =>  1,
+                'password' => Hash::make('Admin123'),
+                'status' => 'active',
+                 'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        ];
+
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+        
     }
 }
